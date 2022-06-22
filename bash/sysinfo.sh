a50d382e1a88048c10df51f6998f84c837ec5340
Dixan <dixanpaul@gmail.com>
2022-06-22 12:22:06 -0400

    updated system information

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,22 +1,42 @@
+#!/bin/bash
+
 #This is the system information of this device.
 
 
-#This displays the hostname of the device.
-echo "Hostname: $(hostname)"
+#This command store the hostname of the device.
+hn=$(hostname)
 
 
-#This displays the name and verson of operating system.
-#We use grep command to retrieve  one line from the whole data provided in the file /etc/os-release.
-echo "Operating system name and version: $(grep PRETTY_NAME /etc/os-release)"
+# This command store domain name if it have one.
+dn=$(dnsdomainname)
 
 
-#This displays ip address of the device.
+#This command store the name and verson of operating system.
+#We use 2 options with the command to get the precise reading.
+os=$(lsb_release -d -s)
+
+
+#This command store ip address of the device.
 #We used grep command to select the line containing ip address and awk command to select the word from the line by passing it through the pipeline.
-echo "IP Address: $(ip a s ens33 | grep -w inet | awk '{print $2}')"
+ip=$(ip a s ens33 | grep -w inet | awk '{print $2}')
 
 
-#This displays the amount of space available in root filesystem.
-#We used df command to display the disc usage and the option -h to display it in human friendly form.
-#Since /dev/sda3 is my root filesystem, I captured that details.
-echo "Root Filesystem Status:"
-df -h /dev/sda3
+#This command stores the amount of free space available in root filesystem.
+#We used df command to display the disc usage, tail to select the last line, and awk to get only the required data.
+freespace=$(df -h /dev/sda3 | tail -1 | awk '{print $4}')
+
+
+#We are calling all variables to display the result.
+cat <<EOF
+
+
+ Report for $hn
+ ======================================================================
+
+ FQDN: $dn
+ Operating System name and version: $os 
+ IP Address: $ip
+ Root Filesystem Free Space: $freespace
+
+ ======================================================================
+EOF
